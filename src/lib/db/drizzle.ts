c2a85d85ecf630409c7a25819ab4a38d8b45f88d
Antonio Ribeiro <antonioribeiroamg@gmail.com>
2025-02-13 08:07:08 -0400

    update

--- src/lib/db/drizzle.ts
+++ src/lib/db/drizzle.ts
@@ -2,20 +2,11 @@ import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 import * as schema from './schema';
 
-const { RDS_HOSTNAME, RDS_PORT, RDS_DB_NAME, RDS_USERNAME, RDS_PASSWORD } =
-  process.env;
+const { POSTGRES_URL } = process.env;
 
-if (
-  !RDS_HOSTNAME ||
-  !RDS_PORT ||
-  !RDS_DB_NAME ||
-  !RDS_USERNAME ||
-  !RDS_PASSWORD
-) {
+if (!POSTGRES_URL) {
   throw new Error('One or more DB environment variables are not set');
 }
 
-const connectionString = `postgres://${RDS_USERNAME}:${RDS_PASSWORD}@${RDS_HOSTNAME}:${RDS_PORT}/${RDS_DB_NAME}`;
-
-export const client = postgres(connectionString);
+export const client = postgres(POSTGRES_URL);
 export const db = drizzle(client, { schema });
